367467cbf2aaa07a00abbf20fae54103b8eb2db2
Ilya Ashikhmin <ilya@ashikhm.in>
2021-12-09 09:17:58 +0300

    Day 9. Clean up

--- day9.py
+++ day9.py
@@ -1,6 +1,5 @@
-from functools import reduce
-
 from aocd.models import Puzzle
+from scipy import ndimage
 
 from snippets import *
 
@@ -8,18 +7,10 @@ puzzle = Puzzle(year=2021, day=9)
 pid = puzzle.input_data
 
 M = np.array([[int(x) for x in line] for line in pid.splitlines()])
-Mp = np.pad(M, 1, constant_values=(9,))
-Mm = reduce(lambda x, y: np.minimum(x, y), [Mp,
-                                            np.roll(Mp, 1, axis=0),
-                                            np.roll(Mp, -1, axis=0),
-                                            np.roll(Mp, 1, axis=1),
-                                            np.roll(Mp, -1, axis=1)])
-Ml = (Mm[1:-1, 1:-1] == M) & (M < 9)
-puzzle.answer_a = Ml.sum() + M[Ml].sum()
 
-from scipy import ndimage
-
-B = ndimage.label(M < 9, structure=np.array([[0, 1, 0],
-                                             [1, 1, 1],
-                                             [0, 1, 0]]))
-puzzle.answer_b = np.sort(np.unique(B[0], return_counts=True)[1])[-4:-1].prod()
+B, Nb = ndimage.label(M < 9, structure=np.array([[0, 1, 0],
+                                                 [1, 1, 1],
+                                                 [0, 1, 0]]))
+beds = [M[B == i].min() for i in range(1, Nb + 1)]
+puzzle.answer_a = sum(beds) + len(beds)
+puzzle.answer_b = np.sort(np.unique(B, return_counts=True)[1])[-4:-1].prod()
